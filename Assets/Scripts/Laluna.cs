8988091d5e4711b45bf2ba28ba1e4c325362e7f1
Damian Niedziolka <95479392+Karmelica@users.noreply.github.com>
2024-03-29 14:42:43 +0100

    1.02

--- Assets/Scripts/Laluna.cs
+++ Assets/Scripts/Laluna.cs
@@ -2,29 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Laluna : MonoBehaviour
 {
     [Header("Components")]
     public Animator animator;
+    public Image danceOff;
+    public Image pills;
+    public Image talkShit;
+    public Image dissTrack;
+
 
     [Header("Objects")]
     public Transform objekt;
 
     private NavMeshAgent meshAgent;
-    private Rigidbody rb;
+
+    public void Bar()
+    {
+
+    }
 
     void Start()
     {
-        rb= GetComponent<Rigidbody>();
         meshAgent = GetComponent<NavMeshAgent>();
         meshAgent.SetDestination(objekt.position);
     }
 
     void Update()
     {
-        animator.SetFloat("Vel", rb.velocity.magnitude);
-        Debug.Log(rb.velocity.magnitude);
+
+
+        Vector3 distance = objekt.position - transform.position;
+        if(distance.magnitude > 2)
+        {
+            meshAgent.isStopped = false;
+            animator.SetBool("isStopped", meshAgent.isStopped);
+            meshAgent.SetDestination(objekt.position);
+        }
+        else
+        {
+            animator.SetBool("isStopped", meshAgent.isStopped);
+            meshAgent.isStopped = true;
+        }
         
     }
 }
