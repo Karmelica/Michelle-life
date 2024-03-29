087796d179d1bc40a592495429fcfdb9610a6e93
Karmelica <damianniedziolka7@gmail.com>
2024-03-29 17:40:04 +0100

    0.02

--- Assets/Scripts/Laluna.cs
+++ Assets/Scripts/Laluna.cs
@@ -8,44 +8,49 @@ public class Laluna : MonoBehaviour
 {
     [Header("Components")]
     public Animator animator;
-    public Image danceOff;
-    public Image pills;
-    public Image talkShit;
-    public Image dissTrack;
+    public Image[] needBar;
+    public float[] needPriority;
 
 
     [Header("Objects")]
-    public Transform objekt;
+    public Transform[] needs;
 
     private NavMeshAgent meshAgent;
 
-    public void Bar()
+    public void Bar(Image bar, float ratio, int i)
     {
+        if(bar.fillAmount > 0)
+        {
+            bar.fillAmount -= Time.deltaTime * 0.05f / ratio;
+        }
+        if(bar.fillAmount < 0.2) {
+            meshAgent.SetDestination(needs[i].position);
+            
+            Vector3 distance = needs[i].position - transform.position;
+            if (distance.magnitude >= 1.5)
+            {
+                meshAgent.isStopped = false;
+                animator.SetBool("isStopped", meshAgent.isStopped);
+            }
+            else
+            {
+                meshAgent.isStopped = true;
+                animator.SetBool("isStopped", meshAgent.isStopped);
+            }
+        }
 
     }
 
     void Start()
     {
         meshAgent = GetComponent<NavMeshAgent>();
-        meshAgent.SetDestination(objekt.position);
     }
 
     void Update()
     {
-
-
-        Vector3 distance = objekt.position - transform.position;
-        if(distance.magnitude > 2)
+        for(int i = 0; i<needBar.Length; i++)
         {
-            meshAgent.isStopped = false;
-            animator.SetBool("isStopped", meshAgent.isStopped);
-            meshAgent.SetDestination(objekt.position);
+            Bar(needBar[i], needPriority[i], i);
         }
-        else
-        {
-            animator.SetBool("isStopped", meshAgent.isStopped);
-            meshAgent.isStopped = true;
-        }
-        
     }
 }
